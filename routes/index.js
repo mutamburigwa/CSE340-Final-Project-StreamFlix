72d83ebd30b809b79b71ed18e47b7ea762072fe5
Takudzwa Mutamburigwa <mut23001@byui.edu>
2024-12-08 12:21:34 +0200

    routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');  // Ensure express is required
+const router = express.Router();
+
+router.use('/', require('./swagger'))
+
+router.get('/', (req, res) => { 
+    //#swagger.tags=['Hello World']
+    res.send('');  // Use parentheses here
+});
+
+router.use('/users', require('./users'));
+
+module.exports = router;
